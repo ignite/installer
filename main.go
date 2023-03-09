da8c71d1c658bcfbd867e29e8646bd417f9309b9
Albert Le Batteux <contact@albttx.tech>
2023-03-09 10:27:32 +0000

    fix: installer repo url

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var version = "0.0.0-src"
 
 func main() {
 	c := handler.DefaultConfig
-	opts.New(&c).Repo("github.com/ignite-hq/installer").Version(version).Parse()
+	opts.New(&c).Repo("github.com/ignite/installer").Version(version).Parse()
 	log.Printf("Default user is '%s', GH token set: %v, listening on %d...", c.User, c.Token != "", c.Port)
 	h := &handler.Handler{Config: c}
 	if err := http.ListenAndServe(":"+strconv.Itoa(c.Port), h); err != nil {
